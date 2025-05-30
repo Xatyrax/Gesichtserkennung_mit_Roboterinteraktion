4ed42c9a1c8cc2b869bba5b33ed869acff9b45c1
Xatyrax <xatyrax_mail>
2025-05-30 12:42:33 +0200

    Meeting Fix 1

--- Server/phandam_functions/room_functions.ts
+++ Server/phandam_functions/room_functions.ts
@@ -30,7 +30,11 @@ export async function SetRoomStatus(roomID:number,Free:Boolean){
     {
         sqlcommand = "Update Rooms set Free = 1 WHERE RoomID = ?";
 
-        Workflow_Starter.tryStartPickFromWatingroom();
+        let PatientenID:any = await sql_execute(`SELECT PatientID FROM Patients_Rooms as PR Join Rooms as R WHERE R.RoomKey = 'W';`);
+        if(PatientenID.length > 0)
+        {
+            Workflow_Starter.tryStartPickFromWatingroom();
+        }
         // PatientFromWatingroom();
     }
     else
